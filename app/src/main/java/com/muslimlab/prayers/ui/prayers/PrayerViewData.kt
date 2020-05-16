a7c77970f35f4293b68c2d0d52598300e6b601a4
Sanny <sanny.segue@grabtaxi.com>
2020-05-16 13:08:44 +0800

    (UI)[PRAY-112] prayers with ui revamp

--- app/src/main/java/com/muslimlab/prayers/ui/prayers/PrayerViewData.kt
+++ app/src/main/java/com/muslimlab/prayers/ui/prayers/PrayerViewData.kt
@@ -5,7 +5,7 @@ import com.muslimlab.prayers.ui.prayers.model.PrayerName
 data class AlarmPref (val prayerCode: PrayerName, val isAlarm : Boolean) {
     companion object {
         val DEFAULT = listOf(
-            AlarmPref(PrayerName.fajr, false),
+            AlarmPref(PrayerName.fajir, false),
             AlarmPref(PrayerName.duhur, false),
             AlarmPref(PrayerName.asr, false),
             AlarmPref(PrayerName.maghrib, false),
--- app/src/main/java/com/muslimlab/prayers/ui/prayers/PrayersFragment.kt
+++ app/src/main/java/com/muslimlab/prayers/ui/prayers/PrayersFragment.kt
@@ -1,16 +1,14 @@
 package com.muslimlab.prayers.ui.prayers
 
 import android.content.Context
-import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.databinding.BindingAdapter
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.GlideBuilder
 import com.muslimlab.prayers.PrayersApplication
-import com.muslimlab.prayers.R
-import com.muslimlab.prayers.databinding.FragmentPrayersBinding
+import com.muslimlab.prayers.databinding.FragmentHomeBinding
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,27 +16,26 @@ import com.muslimlab.prayers.databinding.FragmentPrayersBinding
 class PrayersFragment : Fragment() {
 
     lateinit var viewModel: PrayersViewModel
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        viewModel = PrayersViewModel(
-            PrayerRepositoryFactory
-                .createNetworkPrayerRepository(
-                    context.getSharedPreferences("pref_prayer_data", Context.MODE_PRIVATE),
-                    (activity?.application as PrayersApplication).getRetrofitInstance()
-                ),
-
-            context.getSharedPreferences("pref_prayer_data", Context.MODE_PRIVATE),
-            PrayerAlarmManagerImpl(context)
-        )
-    }
+    lateinit var bindingHandler: PrayerViewBindingHandler
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = FragmentPrayersBinding.inflate(inflater).run {
-        this.vm = viewModel
+    ): View? = FragmentHomeBinding.inflate(inflater).run {
+        viewModel = PrayersViewModel(
+            PrayerRepositoryFactory
+                .createNetworkPrayerRepository(
+                    root.context.getSharedPreferences("pref_prayer_data", Context.MODE_PRIVATE),
+                    (activity?.application as PrayersApplication).getRetrofitInstance()
+                ),
+
+            root.context.getSharedPreferences("pref_prayer_data", Context.MODE_PRIVATE),
+            PrayerAlarmManagerImpl(root.context)
+        )
+
+        bindingHandler = PrayerViewBindingHandler(viewModel, this)
+
         return root
     }
 
@@ -53,8 +50,6 @@ class PrayersFragment : Fragment() {
     }
 
     companion object {
-        fun newInstance(): PrayersFragment {
-            return PrayersFragment()
-        }
+        fun newInstance(): PrayersFragment = PrayersFragment()
     }
 }
